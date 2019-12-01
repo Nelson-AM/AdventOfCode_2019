9bcc860125a541c854441e3164b525bd7760c6f9
Nelson <hello@nelson.science>
2019-12-01 14:02:19 +0100

    Day 1 puzzle 2

--- FuelCounterUpper.py
+++ FuelCounterUpper.py
@@ -10,19 +10,34 @@ def load_file_as_list(filename, to_int=None):
     return list_from_file
 
 
-def calculate_total_fuel_requirements(total_mass):
+def calculate_total_fuel_requirements_modules_only(total_mass):
     total_fuel = 0
     for i in total_mass:
-        total_fuel += calculate_fuel_requirement_for_module(i)
+        total_fuel += calculate_fuel_given_mass(i)
     return total_fuel
 
 
-def calculate_fuel_requirement_for_module(mass):
+def calculate_total_fuel_requirements(total_mass):
+    total_fuel = 0
+    for i in total_mass:
+        total_fuel += calculate_total_fuel_for_module(i)
+    return total_fuel
+
+
+def calculate_total_fuel_for_module(module_mass):
+    total_fuel_list = [calculate_fuel_given_mass(module_mass)]
+    while calculate_fuel_given_mass(total_fuel_list[-1]) > 0:
+        total_fuel_list.append(calculate_fuel_given_mass(total_fuel_list[-1]))
+    return sum(total_fuel_list)
+
+
+def calculate_fuel_given_mass(mass):
     return (int(mass / 3)) - 2
 
 
 if __name__ == "__main__":
     input_file = "Resources/input_day_1"
     input_masses = load_file_as_list(input_file, to_int=True)
-    print("Total fuel required = ",
+
+    print("Total fuel required: ",
           calculate_total_fuel_requirements(input_masses))
--- TestFuelCounterUpper.py
+++ TestFuelCounterUpper.py
@@ -1,7 +1,8 @@
 import unittest
 
-from FuelCounterUpper import calculate_fuel_requirement_for_module, \
-    load_file_as_list, calculate_total_fuel_requirements
+from FuelCounterUpper import calculate_fuel_given_mass, \
+    load_file_as_list, calculate_total_fuel_requirements_modules_only, \
+    calculate_total_fuel_for_module, calculate_total_fuel_requirements
 
 
 class MyTestCase(unittest.TestCase):
@@ -15,20 +16,37 @@ class MyTestCase(unittest.TestCase):
                           load_file_as_list, invalid_filename)
 
     def test_calculate_fuel_requirement_returns_correct_value(self):
-        self.assertEqual(calculate_fuel_requirement_for_module(12), 2)
-        self.assertEqual(calculate_fuel_requirement_for_module(14), 2)
-        self.assertEqual(calculate_fuel_requirement_for_module(1969), 654)
-        self.assertEqual(calculate_fuel_requirement_for_module(100756), 33583)
+        self.assertEqual(calculate_fuel_given_mass(12), 2)
+        self.assertEqual(calculate_fuel_given_mass(14), 2)
+        self.assertEqual(calculate_fuel_given_mass(1969), 654)
+        self.assertEqual(calculate_fuel_given_mass(100756), 33583)
 
-    def test_calculate_total_fuel_req_returns_correct_value(self):
-        self.assertEqual(calculate_total_fuel_requirements([12]), 2)
-        self.assertEqual(calculate_total_fuel_requirements([12, 14]),
-                         2+2)
-        self.assertEqual(calculate_total_fuel_requirements([12, 14, 1969]),
-                         2+2+654)
-        self.assertEqual(calculate_total_fuel_requirements([12, 14,
-                                                            1969, 100756]),
-                         2+2+654+33583)
+    def test_calculate_total_fuel_req_per_module(self):
+        self.assertEqual(2, calculate_total_fuel_for_module(14))
+        self.assertEqual(966, calculate_total_fuel_for_module(1969))
+        self.assertEqual(50346, calculate_total_fuel_for_module(100756))
+
+    def test_calculate_total_fuel_required_returns_correct_value(self):
+        self.assertEqual(2, calculate_total_fuel_requirements([12]))
+        self.assertEqual(2 + 2, calculate_total_fuel_requirements([12, 14]))
+        self.assertEqual(2 + 2 + 966,
+                         calculate_total_fuel_requirements([12, 14, 1969]))
+        self.assertEqual(2 + 2 + 966 + 50346,
+                         calculate_total_fuel_requirements(
+                             [12, 14, 1969, 100756]))
+
+    def test_calculate_total_fuel_req_mod_only_returns_correct_value(self):
+        self.assertEqual(2, calculate_total_fuel_requirements_modules_only(
+            [12]))
+        self.assertEqual(2 + 2,
+                         calculate_total_fuel_requirements_modules_only(
+                             [12, 14]))
+        self.assertEqual(2 + 2 + 654,
+                         calculate_total_fuel_requirements_modules_only(
+                             [12, 14, 1969]))
+        self.assertEqual(2 + 2 + 654 + 33583,
+                         calculate_total_fuel_requirements_modules_only(
+                             [12, 14, 1969, 100756]))
 
 
 if __name__ == '__main__':
