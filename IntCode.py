7b7914ea1079c488ada858fa79214bc6fbbe8fa0
Nelson <hello@nelson.science>
2019-12-02 22:52:54 +0100

    Day 2 puzzle 2

--- IntCode.py
+++ IntCode.py
@@ -12,46 +12,69 @@ def load_one_line_csv_file_as_list(filename, to_int=None):
     return current_line
 
 
-def manipulate_values(input_array, op_code=None):
+def manipulate_values(input_list, op_code=None):
     if not op_code:
-        op_code = input_array[0]
-    index_to_change = input_array[-1]
+        op_code = input_list[0]
+    index_to_change = input_list[-1]
     if op_code == 1:
-        new_value = input_array[1] + input_array[2]
+        new_value = input_list[1] + input_list[2]
     else:
-        new_value = input_array[1] * input_array[2]
-    input_array[index_to_change] = new_value
-    return input_array
+        new_value = input_list[1] * input_list[2]
+    input_list[index_to_change] = new_value
+    return input_list
 
 
-def run_opcodes(input_array):
-    for i in range(0, len(input_array), 4):
-        op_code = input_array[i]
+def run_opcodes(input_list):
+    for i in range(0, len(input_list), 4):
+        op_code = input_list[i]
         if op_code == 99:
-            return input_array
+            return input_list
         else:
-            index_to_change = input_array[i + 3]
+            index_to_change = input_list[i + 3]
 
             if op_code == 1:
-                input_array[index_to_change] = input_array[
-                                                   input_array[i + 1]] + \
-                                               input_array[input_array[i + 2]]
+                input_list[index_to_change] = input_list[
+                                                   input_list[i + 1]] + \
+                                              input_list[input_list[i + 2]]
             elif op_code == 2:
-                input_array[index_to_change] = input_array[
-                                                   input_array[i + 1]] * \
-                                               input_array[input_array[i + 2]]
-    return input_array
+                input_list[index_to_change] = input_list[
+                                                   input_list[i + 1]] * \
+                                              input_list[input_list[i + 2]]
+    return input_list
 
 
-def initialize_to_1202_state(input_array):
-    input_array[1] = 12
-    input_array[2] = 2
-    return input_array
+def initialize_to_state(input_list, noun, verb):
+    input_list[1] = noun
+    input_list[2] = verb
+    return input_list
+
+
+def calculate_puzzle_output(noun, verb):
+    return 100 * noun + verb
+
+
+def find_output_for_target_value(values_to_check, target):
+    for output_values in values_to_check:
+        if output_values[0] == target:
+            return calculate_puzzle_output(output_values[1], output_values[2])
+
+
+def cycle_through_instruction_values(filename, target):
+    output_to_check = []
+    for i in range(0, 99):
+        for j in range(0, 99):
+            list_from_file = load_one_line_csv_file_as_list(filename,
+                                                            to_int=True)
+            initialized = initialize_to_state(list_from_file, i, j)
+            output_list = run_opcodes(initialized)
+            output_to_check.append([output_list[0], i, j])
+    return find_output_for_target_value(output_to_check, target)
 
 
 if __name__ == "__main__":
     input_file = "Resources/input_day_2"
-    list_from_file = load_one_line_csv_file_as_list(input_file, to_int=True)
-    initialized_list = initialize_to_1202_state(list_from_file)
-    op_codes = run_opcodes(initialized_list)
-    print("Initial op_code is: ", op_codes[0])
+    target_value = 19690720
+
+    puzzle_output = cycle_through_instruction_values(input_file, target_value)
+
+    print("Puzzle output: ", puzzle_output)
