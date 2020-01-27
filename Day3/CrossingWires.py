0a96a12aa32b43ac472eb56b685850afffa1c358
Nelson <nelson.mooren@rabobank.nl>
2020-01-27 17:51:41 +0100

    Major update for day 3 part 1.

--- Day3/CrossingWires.py
+++ Day3/CrossingWires.py
@@ -1,37 +1,58 @@
+from Day1.FuelCounterUpper import load_file_as_list
+
+
 def parse_string_to_csv_list(input_string):
     return input_string.split(",")
 
 
-def find_coordinates_of_changes(wire_path):
-    coordinates = [[0, 0]]
-    wire_path_list = parse_string_to_csv_list(wire_path)
+def find_full_path_of_wire(wire_instructions):
+    wire_path_list = parse_string_to_csv_list(wire_instructions)
+
+    full_path = [[0, 0]]
 
     for instruction in wire_path_list:
-        direction = instruction[0]
-        distance = int(instruction[1:])
-        new_coordinates = coordinates[-1]
-        if direction == "R":
-            new_coordinates[0] += distance
-        elif direction == "L":
-            new_coordinates[0] -= distance
-        elif direction == "U":
-            new_coordinates[-1] += distance
-        else:
-            new_coordinates[-1] -= distance
-        coordinates.append(new_coordinates)
-    return coordinates
+        for i in range(int(instruction[1:])):
+            full_path = update_path(full_path, instruction[0])
+    return full_path[1:]
+
+
+def update_path(full_path, direction):
+    new_path = []
+    coordinate = full_path[-1]
+    if direction == "R":
+        new_path = full_path + [[coordinate[0] + 1, coordinate[1]]]
+    elif direction == "L":
+        new_path = full_path + [[coordinate[0] - 1, coordinate[1]]]
+    elif direction == "U":
+        new_path = full_path + [[coordinate[0], coordinate[1] + 1]]
+    elif direction == "D":
+        new_path = full_path + [[coordinate[0], coordinate[1] - 1]]
+    else:
+        print("This state should not be reachable.")
+    return new_path
+
+
+def find_intersections(path_one, path_two):
+    return [value for value in path_one if value in path_two]
+
+
+def calculate_manhattan_distances(intersections):
+    return [abs(value[0]) + abs(value[1]) for value in intersections]
 
 
 def find_closest_intersection(wire_one, wire_two):
-    if wire_one == ["R8,U5,L5,D3"] and wire_two == ["U7,R6,D4,L4"]:
-        return 6
-    elif wire_one == ["R75,D30,R83,U83,L12,D49,R71,U7,L72"]\
-            and wire_two == ["U62,R66,U55,R34,D71,R55,D58,R83"]:
-        return 159
-    else:
-        return 135
+    path_one = find_full_path_of_wire(wire_one)
+    path_two = find_full_path_of_wire(wire_two)
+
+    intersections = find_intersections(path_one, path_two)
+    # print(intersections)
+    manhattan_distances = calculate_manhattan_distances(intersections)
+    return min(manhattan_distances)
 
 
 if __name__ == "__main__":
-    print("Finding intersection between lines.")
-    find_coordinates_of_changes("R8,U5,L5,D3")
+    filename = "Resources/input_day_3.txt"
+    list_from_file = load_file_as_list(filename, to_int=False)
+    closest_intersection = find_closest_intersection(list_from_file[0],
+                                                     list_from_file[1])
+    print("Closest intersection: ", closest_intersection)
--- Day3/TestCrossingWires.py
+++ Day3/TestCrossingWires.py
@@ -1,7 +1,8 @@
 import unittest
 
 from Day3.CrossingWires import find_closest_intersection, \
-    find_coordinates_of_changes, parse_string_to_csv_list
+    find_full_path_of_wire, parse_string_to_csv_list, find_intersections, \
+    calculate_manhattan_distances
 
 
 class MyTestCase(unittest.TestCase):
@@ -12,24 +13,45 @@ class MyTestCase(unittest.TestCase):
                          parse_string_to_csv_list("U7,R6,D4,L4"))
 
     def test_coordinates_of_bends_are_found(self):
-        self.assertEqual([[0, 0], [8, 0], [8, 5], [3, 5], [3, 2]],
-                         find_coordinates_of_changes("R8,U5,L5,D3"))
-        self.assertEqual([[0, 0], [0, 7], [6, 7], [6, 3], [2, 3]],
-                         find_coordinates_of_changes("U7,R6,D4,L4"))
+        self.assertEqual([[1, 0], [2, 0], [3, 0], [4, 0], [5, 0],
+                          [6, 0], [7, 0], [8, 0], [8, 1], [8, 2], [8, 3],
+                          [8, 4], [8, 5], [7, 5], [6, 5], [5, 5], [4, 5],
+                          [3, 5], [3, 4], [3, 3], [3, 2]],
+                         find_full_path_of_wire("R8,U5,L5,D3"))
+        self.assertEqual([[0, 1], [0, 2], [0, 3], [0, 4], [0, 5],
+                          [0, 6], [0, 7], [1, 7], [2, 7], [3, 7], [4, 7],
+                          [5, 7], [6, 7], [6, 6], [6, 5], [6, 4], [6, 3],
+                          [5, 3], [4, 3], [3, 3], [2, 3]],
+                         find_full_path_of_wire("U7,R6,D4,L4"))
+
+    def test_all_intersections_are_found(self):
+        self.assertEqual([[6, 5], [3, 3]],
+                         find_intersections(
+                             [[1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0],
+                              [7, 0], [8, 0], [8, 1], [8, 2], [8, 3], [8, 4],
+                              [8, 5], [7, 5], [6, 5], [5, 5], [4, 5], [3, 5],
+                              [3, 4], [3, 3], [3, 2]],
+                             [[0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6],
+                              [0, 7], [1, 7], [2, 7], [3, 7], [4, 7], [5, 7],
+                              [6, 7], [6, 6], [6, 5], [6, 4], [6, 3], [5, 3],
+                              [4, 3], [3, 3], [2, 3]]))
+
+    def test_manhattan_distances_calculated(self):
+        self.assertEqual([11, 6],
+                         calculate_manhattan_distances([[6, 5], [3, 3]]))
 
     def test_closest_intersection_is_found(self):
-        # Crosses at [3, 3] and [6, 5]
         self.assertEqual(6,
-                         find_closest_intersection(["R8,U5,L5,D3"],
-                                                   ["U7,R6,D4,L4"]))
+                         find_closest_intersection("R8,U5,L5,D3",
+                                                   "U7,R6,D4,L4"))
         self.assertEqual(159,
                          find_closest_intersection(
-                             ["R75,D30,R83,U83,L12,D49,R71,U7,L72"],
-                             ["U62,R66,U55,R34,D71,R55,D58,R83"]))
+                             "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+                             "U62,R66,U55,R34,D71,R55,D58,R83"))
         self.assertEqual(135,
                          find_closest_intersection(
-                             ["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"],
-                             ["U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]))
+                             "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+                             "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"))
 
 
 if __name__ == '__main__':
